afb966033c867e0940dade4d65336a2b87f8c864
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-04-27 14:49:44 -0300

    formatação

--- exemplo-request.js
+++ exemplo-request.js
@@ -1,11 +1,12 @@
 var request = require('request');
+
 var options = {
-  'method': 'GET',
-  'url': 'http://localhost:3000',
-  'headers': {
-  }
+    'method': 'GET',
+    'url': 'http://localhost:3000',
+    'headers': {}
 };
+
 request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
+    if (error) throw new Error(error);
+    console.log(response.body);
 });
--- exemplo-unirest.js
+++ exemplo-unirest.js
@@ -1,7 +1,7 @@
 var unirest = require('unirest');
 
 var req = unirest('GET', 'http://localhost:3000')
-  .end(function (res) {
-    if (res.error) throw new Error(res.error);
-    console.log(res.raw_body);
-  });
+    .end(function (res) {
+        if (res.error) throw new Error(res.error);
+        console.log(res.raw_body);
+    });
