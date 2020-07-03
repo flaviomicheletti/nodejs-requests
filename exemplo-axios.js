3cac722fc04144efcc9a4f160be5546260006a54
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-03 10:32:37 -0300

    exemplo axios

--- exemplo-axios.js
+++ exemplo-axios.js
@@ -11,21 +11,63 @@ const config = {
 //
 axios(config)
     .then(function (response) {
-        console.log(response.data);
+        console.log('a)', response.data.length);
     })
     .catch(function (error) {
         console.log(error);
     });
 
+
 //
 // async / await
 //
 (async () => {
-    let res = await axios.get(config.url);
-    console.log(res.data);
+    let response = await axios.get(config.url);
+    console.log('b)', response.data.length);
 })()
 
-// request = async () => {
-//     let res = await axios.get(config.url);
-//     return res.data;
-// };
+
+//
+// async / await
+//
+const request = async () => {
+    let response = await axios.get(config.url);
+    console.log('c)', response.data.length);
+    return response.data;
+};
+
+request().then(response => console.log('d)', response.length));
+
+
+//
+// async / await
+//
+async function getData() {
+    try {
+        let response = await axios.get(config.url);
+        console.log('e)', response.data.length);
+        const data = await response.data;
+        return data;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+getData().then(response => console.log('f)', response.length));
+
+
+//
+// async / await
+//
+const sendGetRequest = async () => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        console.log('g)', response.data.length);
+        return response.data;
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+sendGetRequest().then(response => console.log('f)', response.length));
